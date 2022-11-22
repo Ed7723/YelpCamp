c79a4fa85e5f90ddda5ada3c7fa16cd6bb91e10c
Ed7723 <linde2222@hotmail.com>
2022-11-21 23:55:06 -0500

    Update: Generated 1000 new campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const cities = require('./cities')
 const Campground = require('./models/campground')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
@@ -11,3 +12,15 @@ db.on("error",console.error.bind(console,"connection error:"));
 db.once("open", ()=>{
     console.log("Database connected");
 });
+
+const seedDB = async() =>{
+    await Campground.deleteMany({});
+    for(let i=0;i<50;i++){
+        const random1000 =Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location:`${cities[random1000].city}, ${cities[random1000].state}`
+        })
+        await camp.save()
+    }
+}
+seedDB();
