7593ec781b90cda75249b0cf6fa8fb6f2611b59f
Ed7723 <linde2222@hotmail.com>
2022-11-22 00:01:44 -0500

    Update: Added descriptors and close db to seeding

--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const mongoose = require('mongoose')
 const cities = require('./cities')
 const Campground = require('./models/campground')
-
+const {places,descriptors} = require('./seedhelpers')
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
 });
 
@@ -12,15 +12,23 @@ db.on("error",console.error.bind(console,"connection error:"));
 db.once("open", ()=>{
     console.log("Database connected");
 });
+const sample = (array) =>{
+    array[Math.floor(Math.random()*array.length)]
+};
 
+array[Math.floor(Math.random()*array.length)];
 const seedDB = async() =>{
     await Campground.deleteMany({});
     for(let i=0;i<50;i++){
         const random1000 =Math.floor(Math.random() * 1000);
         const camp = new Campground({
-            location:`${cities[random1000].city}, ${cities[random1000].state}`
+            location:`${cities[random1000].city}, ${cities[random1000].state}`,
+            title:`${sample(descriptors)} ${sample(places)}`
         })
         await camp.save()
     }
 }
-seedDB();
+
+seedDB().then(()=>{
+    mongoose.connection.close()
+})
