583772c103166a4f18cffbfb49d1940c1d753dbd
Ed7723 <linde2222@hotmail.com>
2022-12-28 13:38:13 -0800

    Update: Fixed bugs so all CRUD works

--- app.js
+++ app.js
@@ -33,26 +33,27 @@ app.get('/campgrounds', async(req,res)=>{
 app.get('/campgrounds/new', (req,res)=>{
     res.render('campgrounds/new');
 })
+
 app.post('/campgrounds', async(req,res)=>{
     const campground = new Campground(req.body.campground);
     await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
+    res.redirect(`campgrounds/${campground._id}`)
 })
 
-app.get('/campgrouds/:id',async(req,res)=>{
-    const campgroud = await Campground.findById(req.params.id);
-    res.render('campgrounds/show', {campgroud})
+app.get('/campgrounds/:id',async(req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {campground})
 })
 
 app.get(`/campgrounds/:id/edit`, async(req,res)=>{
-    const campgroud = await Campground.findById(req.params.id);
-    res.render('campgrounds/edit', {campgroud})
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground})
 })
 
 app.listen(3000, ()=>{
     console.log('Serving on port 3000')
 })
-app.delete('/campgrounds/id',async(req,res)=>{
+app.delete('/campgrounds/:id',async(req,res)=>{
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
@@ -60,5 +61,5 @@ app.delete('/campgrounds/id',async(req,res)=>{
 app.put(`/campgrounds/:id`,async(req,res)=>{
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
-    res.redirect(`/campgrounds/${campground_id}`);
+    res.redirect(`/campgrounds/${campground._id}`);
 })
