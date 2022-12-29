daad553b985ab5f6f47a2e8c0ba18c8dec69f5a5
Ed7723 <linde2222@hotmail.com>
2022-12-29 14:55:20 -0800

    Create: Added random photos and descriptor

--- seeds/index.js
+++ seeds/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const cities = require('./cities')
 const Campground = require('../models/campground')
 const {places,descriptors} = require('./seedhelpers')
+const { default: axios } = require('axios')
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
 });
 
@@ -14,13 +15,35 @@ db.once("open", ()=>{
 });
 const sample = array => array[Math.floor(Math.random()*array.length)];
 
+//This function essentially uses the unsplash API and gets the image link using a GET request
+//through axios, in the seedDB function we will call it and get the image url to use.
+//client id is the accesskey
+async function seedImg(){
+    try{
+        const resp = await axios.get('https://api.unsplash.com/photos/random',{
+            params:{
+                client_id: '',
+                collections: 483251,
+            },
+        })
+        return resp.data.urls.small
+    } catch(err){
+        console.error(err)
+    }
+}
+
 const seedDB = async() =>{
     await Campground.deleteMany({});
-    for(let i=0;i<50;i++){
+    for(let i=0;i<10;i++){
         const random1000 =Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random()*30) + 10;
+        const images = await seedImg();
         const camp = new Campground({
+            image : images,
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
             title:`${sample(descriptors)} ${sample(places)}`,
+            description: "Random description",
+            price
         });
         await camp.save();
     };
