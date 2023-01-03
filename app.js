67a3cb91979aa0cc89a98d48f31a7063c45dc27e
Ed7723 <linde2222@hotmail.com>
2023-01-03 14:43:43 -0800

    Create: Basic async error handling

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require('express')
-const path = require('path')
-const mongoose = require('mongoose')
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
 const methoOverride = require('method-override');
-const ejsMate = require('ejs-mate')
-const Campground = require('./models/campground')
+const ejsMate = require('ejs-mate');
+const catchAsync = require('./utils/catchAsync');
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
 });
@@ -25,41 +26,47 @@ app.get('/', (req,res)=>{
     res.render('Home')
 })
 
-app.get('/campgrounds', async(req,res)=>{
+app.get('/campgrounds', catchAsync(async(req,res)=>{
     const allCamps = await Campground.find({});
     res.render('campgrounds/index', {allCamps})
-})
+}))
 
 app.get('/campgrounds/new', (req,res)=>{
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds', async(req,res)=>{
+app.post('/campgrounds',catchAsync(async (req,res,next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`campgrounds/${campground._id}`)
-})
+}))
 
-app.get('/campgrounds/:id',async(req,res)=>{
+app.get('/campgrounds/:id', catchAsync(async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground})
-})
+}))
 
-app.get(`/campgrounds/:id/edit`, async(req,res)=>{
+app.get(`/campgrounds/:id/edit`, catchAsync(async(req,res)=>{
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', {campground})
+}))
+
+app.delete('/campgrounds/:id',catchAsync(async(req,res)=>{
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+}))
+
+app.put(`/campgrounds/:id`,catchAsync(async(req,res)=>{
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+app.use((err,req,res,next)=>{
+    res.send('Sum ting wong')
 })
 
 app.listen(3000, ()=>{
     console.log('Serving on port 3000')
-})
-app.delete('/campgrounds/:id',async(req,res)=>{
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-})
-app.put(`/campgrounds/:id`,async(req,res)=>{
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
-    res.redirect(`/campgrounds/${campground._id}`);
 })
