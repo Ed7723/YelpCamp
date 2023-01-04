2c5180c867125e81ca820cdeac44bf2b0809560e
Ed7723 <linde2222@hotmail.com>
2023-01-03 22:24:17 -0800

    Update: joi middleware

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methoOverride = require('method-override');
-const Joi = require('joi');
+const{campgroundSchema} = require('./schemas');
 const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
@@ -25,6 +25,18 @@ app.use(express.urlencoded({extended:true}));
 app.use(methoOverride(`_method`));
 app.engine('ejs',ejsMate);
 
+const validateCampground = (req,res,next)=> {
+    //Joi is server side validation.
+    const {error} = campgroundSchema.validate(req.body);
+
+    if(error){
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg,400);
+    } else{
+        next();
+    }
+}
+
 app.get('/', (req,res)=>{
     res.render('Home')
 })
@@ -38,24 +50,8 @@ app.get('/campgrounds/new', (req,res)=>{
     res.render('campgrounds/new');
 })
 
-app.post('/campgrounds',catchAsync(async (req,res,next) => {
+app.post('/campgrounds',validateCampground,catchAsync(async (req,res,next) => {
     //if(!req.body.Campground) throw new ExpressError("Invalid Campground Data",400);
-    //Joi is server side validation.
-    const campgroundSchema = Joi.object({
-        campground: Joi.object({
-            title: Joi.string().required(),
-            price: Joi.number().required().min(0),
-            image: Joi.string().required(),
-            location: Joi.string().required(),
-            description: Joi.string().required()
-        }).required()
-    })
-    const {error} = campgroundSchema.validate(req.body);
-
-    if(error){
-        const msg = error.details.map(el => el.message).join(',');
-        throw new ExpressError(msg,400);
-    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`campgrounds/${campground._id}`)
@@ -77,7 +73,7 @@ app.delete('/campgrounds/:id',catchAsync(async(req,res)=>{
     res.redirect('/campgrounds');
 }))
 
-app.put(`/campgrounds/:id`,catchAsync(async(req,res)=>{
+app.put(`/campgrounds/:id`,validateCampground,catchAsync(async(req,res)=>{
     const {id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
     res.redirect(`/campgrounds/${campground._id}`);
