c9f33f6db068dfe4c796a23f95b59246904f0fe0
Ed7723 <linde2222@hotmail.com>
2023-01-07 22:00:38 -0500

    Update: Reviews routing

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 const { join } = require('path');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
@@ -67,15 +68,24 @@ app.get(`/campgrounds/:id/edit`, catchAsync(async(req,res)=>{
     res.render('campgrounds/edit', {campground})
 }))
 
+app.put(`/campgrounds/:id`,validateCampground,catchAsync(async(req,res)=>{
+    const {id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
 app.delete('/campgrounds/:id',catchAsync(async(req,res)=>{
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 }))
 
-app.put(`/campgrounds/:id`,validateCampground,catchAsync(async(req,res)=>{
-    const {id} = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+app.post('/campgrounds/:id/reviews', catchAsync(async(req,res)=>{
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
