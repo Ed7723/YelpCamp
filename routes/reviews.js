e806330d9ce3cc43924135ef00664d5f75f9e6fd
Ed7723 <linde2222@hotmail.com>
2023-01-13 15:48:17 -0500

    Update: flash error

--- routes/reviews.js
+++ routes/reviews.js
@@ -26,7 +26,7 @@ router.post('/',validateReview, catchAsync(async(req,res)=>{
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
-router.delete('/reviewId',catchAsync(async(req,res)=>{
+router.delete('/:reviewId',catchAsync(async(req,res)=>{
     const{id,reviewId} = req.params;
     await Campground.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
     await Review.findByIdAndDelete(reviewId);
