abddc01b92244b6ca763464a4776e7d5854e3709
Ed7723 <linde2222@hotmail.com>
2023-02-21 02:23:33 -0500

    Create: Mapbox popup

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -7,5 +7,11 @@ const map = new mapboxgl.Map({
 });
 
 new mapboxgl.Marker()
-.setLngLat(campground.geometry.coordinates)
-.addTo(map)
+    .setLngLat(campground.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({offset:25})
+        .setHTML(
+            `<h4>${campground.title}</h4><p>${campground.location}</p>`
+        )
+    )
+    .addTo(map)
