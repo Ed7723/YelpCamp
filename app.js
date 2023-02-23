17f29cd6fc1e1431ab9dfd889ea9783de4e2fd69
Ed7723 <linde2222@hotmail.com>
2023-02-23 15:42:32 -0500

    Update: CSP and helmet installation

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
@@ -29,6 +30,31 @@ db.once("open", ()=>{
 });
 const app = express();
 
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/",
+  ];
+  const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/", 
+  ];
+  const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+  ];
+  const fontSrcUrls = ["https://res.cloudinary.com/dzsvvrwtd/"];
+
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname,'views'));
 app.use(express.urlencoded({extended:true}));
@@ -36,8 +62,29 @@ app.use(methoOverride(`_method`));
 app.engine('ejs',ejsMate);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(mongoSanitize());
+app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: [],
+        connectSrc: ["'self'", ...connectSrcUrls],
+        scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+        styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+        workerSrc: ["'self'", "blob:"],
+        objectSrc: [],
+        imgSrc: [
+          "'self'",
+          "blob:",
+          "data:",
+          "https://res.cloudinary.com/dzsvvrwtd/", 
+          "https://images.unsplash.com/",
+        ],
+        fontSrc: ["'self'", ...fontSrcUrls],
+      },
+    })
+  );
 
 const sessionConfig = {
+    name: 'sesson',
     secret:'thisshouldbeabettersecret',
     resave: false,
     saveUninitialized: true,
@@ -45,6 +92,7 @@ const sessionConfig = {
         expires: Date.now() + 1000*60*60*24*7,
         maxAge: 1000*60*60*24*7,
         httpOnly: true,
+        //secure: true
     }
 }
 app.use(session(sessionConfig));
